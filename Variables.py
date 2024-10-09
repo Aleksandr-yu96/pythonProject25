a86fa03ec97294dc9da4345dd58fbfc497098c1d
Aleksandr-yu96 <samuskov_96@mail.ru>
2024-10-10 00:15:38 +0700

    Variables

---
+++ Variables.py
@@ -0,0 +1,5 @@
+homework_count = 12
+Number_of_hours_spent = 1.5
+Course_name = 'python'
+time_per_task = Number_of_hours_spent / homework_count
+print(f"Курс: {Course_name}, всего задач: {homework_count}, затрачено часов: {Number_of_hours_spent}, среднее время выполнения {time_per_task} часа.")
